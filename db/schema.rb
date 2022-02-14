aa692c1d382842ef371bdff1ffa767ac14fe736b
Rawan <rawanalex15@gmail.com>
2022-02-15 00:34:12 +0200

    Add Price Column to Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_13_035659) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_14_222914) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "price"
   end
 
   create_table "users", force: :cascade do |t|
