a53da53f4c177ec642a8d1f4867d16d272508507
Rawan <rawanalex15@gmail.com>
2022-02-18 08:01:59 +0200

    temp job call from config.ru on server start and temp loop in job class

--- app/jobs/daily_summary_job.rb
+++ app/jobs/daily_summary_job.rb
@@ -2,9 +2,11 @@ class DailySummaryJob < ApplicationJob
   queue_as :default
 
   def perform
-    puts"somethingelse"
     items = Item.where("created_at >= :start_date", {start_date: Date.today})
     users = User.where("created_at >= :start_date", {start_date: Date.today})
     DailySummaryMailer.with(items: items, users: users).new_summary_email.deliver_now
+    
+    # A temporary loop by calling the same job until setting the schedule
+    DailySummaryJob.set(wait_until: Date.tomorrow.noon).perform_later
   end
 end
